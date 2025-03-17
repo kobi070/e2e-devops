9be3a266e241be66cda451d01cb51b4a0b5cc858
Kobi <kobi070@gmail.com>
2025-03-17 11:16:49 +0200

    feature/terraform fixing issues with configuration of ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,8 +8,8 @@ provider "aws" {
 resource "aws_instance" "ec2-ubuntu" {
   ami                         = var.ubuntu_ami # Ubuntu AMI
   instance_type               = var.instance_type # t3.medium
-  subnet_id                   = data.aws_subnets.selected.id # Take from data.tf
-  key_name                    = var.ssh_key_name # Take from key.tf
+  subnet_id                   = data.aws_subnets.selected.ids[0] # Take from data.tf
+  key_name                    = aws_key_pair.builder_key.key_name # Take from key.tf
   associate_public_ip_address = true
   security_groups             = [aws_security_group.sg.id]
 
