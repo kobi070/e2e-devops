31eeae1741ba431aa4c875f6fc959313aff596f7
Kobi <kobi070@gmail.com>
2025-03-17 10:27:06 +0200

    feature/terraform init commit of output.tf

---
+++ terraform/output.tf
@@ -0,0 +1,14 @@
+output "ec2_ip" {
+  description = "The Kobi-builder public ip"
+  value       = aws_instance.ec2-ubuntu.public_ip
+}
+
+output "ssh_key_location" {
+  description = "The SSH key location"
+  value       = aws_key_pair.builder_key.key_name
+}
+
+output "sg_id" {
+  description = "The security group ID"
+  value       = aws_security_group.sg.id
+}
