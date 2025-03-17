a0b8b6c8d1d6d801e10f30b6b551b37f5d998baf
Kobi <kobi070@gmail.com>
2025-03-17 10:23:33 +0200

    feature/terraform init commit of key.tf

---
+++ terraform/key.tf
@@ -0,0 +1,30 @@
+# Generate an SSH key pair
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Save the private key locally
+resource "local_file" "private_key" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "${path.module}/builder_key.pem"
+  file_permission = "0600"
+}
+
+# Create an AWS key pair using the public key
+resource "aws_key_pair" "builder_key" {
+  key_name   = "builder-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+# Output the necessary details
+output "ssh_private_key_path" {
+  value       = local_file.private_key.filename
+  description = "Path to the generated private SSH key"
+  sensitive   = true
+}
+
+output "ssh_key_name" {
+  value       = aws_key_pair.builder_key.key_name
+  description = "Name of the AWS SSH key pair"
+}
